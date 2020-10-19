23f9dfe8550f739f425541c41025da09bd46b377
John Nederveld <john.nederveld@gmail.com>
2020-10-18 23:02:28 -0400

    reformatted index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,6 @@ router.get("/", function(req, res) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
@@ -30,10 +29,6 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-  console.log(req.body.devour);
-
   burger.update({
     devoured: req.body.devour
   }, condition, function(result) {
